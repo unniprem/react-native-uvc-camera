f47cf8d293efe12ef9aad351125177c16400821a
Li Zheng <flyskywhy@gmail.com>
2018-06-17 22:31:08 +0800

    just copy some of android/ and src/ from react-native-camera@1.1.4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-export { };
+import Camera from './Camera';
+import RNCamera from './RNCamera';
+import FaceDetector from './FaceDetector';
+
+export { RNCamera, FaceDetector };
+
+export default Camera;
