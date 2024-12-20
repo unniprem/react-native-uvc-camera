16fdc5c2bd1dc1ea09b5556bdba5e8b314bdd6c3
unnikrishnan c <unni.prem@gmail.com>
2024-12-20 20:01:40 +0530

    added

--- libuvccamera/src/main/jni/Application.mk
+++ libuvccamera/src/main/jni/Application.mk
@@ -26,7 +26,7 @@
 # Note: Supporting GCC on NDK is already deprecated and GCC will be removed from NDK soon.
 #NDK_TOOLCHAIN_VERSION := 4.9
 
-APP_PLATFORM := android-14
-APP_ABI := arm64-v8a armeabi-v7a x-86 arm64
+APP_PLATFORM := android-21
+APP_ABI := all
 #APP_OPTIM := debug
 APP_OPTIM := release
