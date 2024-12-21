16088a6477dfa415ec9d7db7ee194730ffa02557
unnikrishnan c <unni.prem@gmail.com>
2024-12-21 12:39:18 +0530

    minor fix

--- libuvccamera/src/main/jni/Android.mk
+++ libuvccamera/src/main/jni/Android.mk
@@ -5,3 +5,10 @@ include $(PROJ_PATH)/UVCCamera/Android.mk
 include $(PROJ_PATH)/libjpeg-turbo-1.5.0/Android.mk
 include $(PROJ_PATH)/libusb/android/jni/Android.mk
 include $(PROJ_PATH)/libuvc/android/jni/Android.mk
+ifneq ($(TARGET_ARCH_ABI), mips)
+    # Include the libjpeg-turbo sources
+    include $(CLEAR_VARS)
+    LOCAL_MODULE := jpeg-turbo1500_static
+    LOCAL_SRC_FILES := jdhuff.c jchuff.c jdphuff.c
+    include $(BUILD_STATIC_LIBRARY)
+endif
