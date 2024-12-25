c196131039c3be8d9764a34b7cf3fb6681ee6eb2
unnikrishnan c <unni.prem@gmail.com>
2024-12-25 16:32:49 +0530

    added

--- libuvccamera/src/main/java/com/serenegiant/usb/USBMonitor.java
+++ libuvccamera/src/main/java/com/serenegiant/usb/USBMonitor.java
@@ -1,79 +1,47 @@
-// 1. First create the USBMonitor class if it's missing
-// File: com/serenegiant/usb/USBMonitor.java
+// File: USBMonitor.java
+// Path: libuvccamera/src/main/java/com/serenegiant/usb/USBMonitor.java
 
 package com.serenegiant.usb;
 
-import android.app.PendingIntent;
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.app.PendingIntent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class USBMonitor {
     private final Context context;
     private final UsbManager usbManager;
+    private List<UsbDevice> deviceList;
     
     public USBMonitor(final Context context) {
         this.context = context;
         this.usbManager = (UsbManager)context.getSystemService(Context.USB_SERVICE);
+        this.deviceList = new ArrayList<>();
     }
     
-    // Basic implementation of required methods
     public void register() {
-        // Implementation for registering USB monitor
+        // Implementation
     }
     
     public void unregister() {
-        // Implementation for unregistering USB monitor
+        // Implementation
+    }
+    
+    public boolean isRegistered() {
+        return true;  // Implement actual logic
     }
     
     public void requestPermission(UsbDevice device) {
-        // Implementation for requesting USB permission
-    }
-}
-
-// 2. Modified CameraDialog.java
-package com.serenegiant.usb_libuvccamera;
-
-import android.app.Activity;
-import android.app.DialogFragment;
-import android.hardware.usb.UsbDevice;
-import com.serenegiant.usb.USBMonitor;
-
-public class CameraDialog extends DialogFragment {
-    private USBMonitor mUSBMonitor;
-    
-    public static CameraDialog showDialog(final Activity parent, USBMonitor usbMonitor) {
-        CameraDialog dialog = newInstance(usbMonitor);
-        dialog.show(parent.getFragmentManager(), "CameraDialog");
-        return dialog;
+        // Implementation
     }
     
-    public static CameraDialog newInstance(USBMonitor usbMonitor) {
-        CameraDialog dialog = new CameraDialog(usbMonitor);
-        return dialog;
+    public List<UsbDevice> getDeviceList() {
+        return deviceList;
     }
     
-    public CameraDialog(USBMonitor usbMonitor) {
-        this.mUSBMonitor = usbMonitor;
+    public Context getContext() {
+        return context;
     }
 }
-
-// 3. Update your build.gradle (app level)
-/*
-android {
-    ...
-    sourceSets {
-        main {
-            java {
-                srcDirs = ['src/main/java', 'src/main/java/com/serenegiant/usb']
-            }
-        }
-    }
-}
-
-dependencies {
-    implementation fileTree(dir: 'libs', include: ['*.jar'])
-    implementation project(':libuvccamera')
-    // Add any other required dependencies
-}
-*/
